8e68575557360f7752d496428d85c63270f7bc48
Ranjitha-T <60682722+Ranjitha-T@users.noreply.github.com>
2020-07-15 17:57:50 +0530

    initial commit

---
+++ program.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+void  main()
+{
+	int i,j,rows;
+	printf("enter the rows\n");
+	scanf("%d",&rows);
+	for(i=1;i<=rows;i++)
+	{
+		for(j=1;j<=i;j++)
+		
+			printf("01");
+			
+		  printf("\n");
+	}
+	
+}
